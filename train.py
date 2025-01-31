e72a5606a87bf01a2e93bf84fe3d8557c6ec887b
SEdeepL <153978170+SEdeepL@users.noreply.github.com>
2025-02-01 01:56:49 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,16 @@
 import torch
+import os
 from peft import LoraConfig, get_peft_model 
-from transformers import TrainingArguments, AutoModelForCausalLM, AutoTokenizer
+from transformers import TrainingArguments, AutoModelForCausalLM, AutoTokenizer,AutoModelForSequenceClassification
 from trl import SFTConfig, SFTTrainer
 from datasets import Dataset
 import ipdb
 train_dataset=[]
 val_dataset=[]
+model_name ='./llama2-hf/'
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token_id = 0
+tokenizer.padding_side = 'right'
 def get_graph(path):
     graph= open(path)
     graph_line = graph.readlines()
@@ -27,7 +32,7 @@ def get_graph(path):
         attri=[]
         for k in range(1,len(cont)):
             attri.append(cont[i].split("=")[1])
-        attri.append(attri)
+        attri.append(eval(attri))
     heads=[]
     tails=[]
     for j in range(edgep+1,len(graph_line)):
@@ -44,26 +49,36 @@ tf = open("./data/train.txt")
 graph_train="./data/graph_train"
 entries = os.listdir(graph_train)
 tfgraph = [d for d in entries if os.path.isdir(os.path.join(graph_train, d))]
-line = tf.readline()
+line = tf.readlines()
 for i in range(len(line)):
     patch,result,_=line[i].split("	")
     label = eval(result)
     train_graph = tfgraph[i]
     nodes, edges,attris =  get_graph(graph_train+tfgraph[i])
-    train_dataset.append({'text': '<s>'+'Assess whether the code is correct:\nInput:'+ patch + '\nOutput:'+ result + '</s>',"nodes":nodes,"edges":edges,"attri":attris,"labels":label})
+    nodeid = []
+    for i in range(len(nodes)):
+        nodeid.append(tokenizer(nodes[i])["input_ids"])
+    edgesid = torch.tensor(edges)
+    attrisid = torch.tensor(attris)
+    train_dataset.append({'text': '<s>'+'Assess whether the code is correct:\nInput:'+ patch +'</s>',"nodes":nodeid,"edges":edgesid,"attri":attrisid,"labels":label})
     line = tf.readline()
 tf.close()
 ef = open("./data/eval.txt") 
 graph_eval="./data/graph_eval"
 entries = os.listdir(graph_train)
 efgraph = [d for d in entries if os.path.isdir(os.path.join(graph_eval, d))]
-line = ef.readline()
+line = ef.readlines()
 for i in range(len(line)):
     patch,result,_=line.split("	")
     label = eval(result)
     val_graph = efgraph[i]
     nodes, edges,attris =  get_graph(graph_train+efgraph[i])
-    val_dataset.append({'text': '<s>'+'Assess whether the code is correct:\nInput:'+ patch + '\nOutput:'+ result +'</s>',"nodes":nodes,"edges":edges,"attri":attris,"labels":label})
+    nodeid = []
+    for i in range(len(nodes)):
+        nodeid.append(tokenizer(nodes[i])["input_ids"])
+    edgesid = torch.tensor(edges)
+    attrisid = torch.tensor(attris)
+    val_dataset.append({'text': '<s>'+'Assess whether the code is correct:\nInput:'+ patch +'</s>',"nodes":nodeid,"edges":edgesid,"attri":attrisid,"labels":label})
     line = ef.readline()
 ef.close()
 train_dataset = Dataset.from_dict({key: [dic[key] for dic in train_dataset] for key in train_dataset[0]})
@@ -72,9 +87,7 @@ def preprocess_function(examples):
     return tokenizer(examples['text'], truncation=True, padding=True)
 
 encoded_train_dataset = train_dataset.map(preprocess_function, batched=True)
-encoded_train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'nodes','edges','attris','labels'])
 encoded_eval_dataset = val_dataset.map(preprocess_function, batched=True)
-encoded_eval_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'nodes','edges','attris','labels'])
 # ipdb.set_trace()
 peft_config = LoraConfig(
     r=8,
@@ -103,11 +116,12 @@ training_aruments = TrainingArguments(
     lr_scheduler_type='cosine',
     warmup_steps=100
 )
-model_name ='./llama2-hf/'
-model = AutoModelForCausalLM.from_pretrained(
+
+model = AutoModelForSequenceClassification.from_pretrained(
     model_name,
     torch_dtype=torch.float16,
-    device_map='auto'
+    device_map='auto',
+    num_labels=2
 )
 
 
@@ -116,9 +130,7 @@ model.enable_input_require_grads()
 model = get_peft_model(model, peft_config)
 model.print_trainable_parameters()
 model.config.use_cache = False
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token_id = 0
-tokenizer.padding_side = 'right'
+
 # ipdb.set_trace()
 trainer = SFTTrainer(
     model=model,
